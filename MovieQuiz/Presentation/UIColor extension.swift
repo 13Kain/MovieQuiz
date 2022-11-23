faeda54130b63fc584dc00e840fbd75a2ef29743
13Kain <98162781+13Kain@users.noreply.github.com>
2022-11-23 13:39:01 +0300

    final

---
+++ MovieQuiz/Presentation/UIColor extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor extension.swift
+//  MovieQuiz
+//
+//  Created by Никита on 23.11.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "YP Black")!}
+    static var ypWhite: UIColor { UIColor(named: "YP White")!}
+    static var ypDarkGray: UIColor { UIColor(named: "YP DarkGray")!}
+    static var ypGreen: UIColor { UIColor(named: "YP Green")!}
+    static var ypRed: UIColor { UIColor(named: "YP Red")!}
+    static var ypGray: UIColor { UIColor(named: "YP Gray")!}
+    
+}
